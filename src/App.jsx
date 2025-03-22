bcdeaa36c3c99e7d64f91174ed365cae6f652bed
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2025-03-22 16:24:24 +0200

    Restructure paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { ThemeProvider, CssBaseline, useMediaQuery } from '@mui/material';
 import { lightTheme, darkTheme } from './styles/themes';
 
-import UserList from './components/UserList';
+import UserList from './components/user-list/UserList';
 
 export default function App() {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
